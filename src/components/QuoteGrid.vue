0721ffe99a5a3f570269b26d83ac0f530da2a52b
전민승 <jeon@jeonminseungs-MacBook-Pro.local>
2021-02-15 17:11:47 +0900

    10-09. Allowing for Quote Deletion

--- src/components/QuoteGrid.vue
+++ src/components/QuoteGrid.vue
@@ -1,20 +1,28 @@
 <template>
-    <div class="row">
-        <app-quote v-bind:key="quote" v-for="quote in quotes">{{ quote }}</app-quote>
-    </div>
+  <div class="row">
+    <app-quote
+      v-bind:key="index"
+      v-for="(quote, index) in quotes"
+      @click.native="deleteQuote(index)"
+      >{{ quote }}</app-quote
+    >
+  </div>
 </template>
 
 <script>
-    import Quote from './Quote.vue';
+import Quote from "./Quote.vue";
 
-    export default {
-        props: ['quotes'],
-        components: {
-            appQuote: Quote
-        }
-    }
+export default {
+  props: ["quotes"],
+  components: {
+    appQuote: Quote,
+  },
+  methods: {
+    deleteQuote(index) {
+      this.$emit("quoteDeleted", index);
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
