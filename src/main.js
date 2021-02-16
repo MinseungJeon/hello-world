df5fc73027aec7758c842f86f14b4cd93660260a
전민승 <jeon@jeonminseungs-MacBook-Pro.local>
2021-02-16 11:40:17 +0900

    13-02. Creating a Local Filter

--- src/main.js
+++ src/main.js
@@ -11,24 +11,7 @@ import App from './App.vue'
 
 // Vue.config.productionTip = false
 
-Vue.directive('highlight', {
-  bind(el, binding, vnode){
-    // el.style.backgroundColor = 'green';
-    // el.style.backgroundColor = binding.value;
-    let delay = 0;
-    if (binding.modifiers['delayed']){
-      delay = 3000;
-    }
-    setTimeout(() => {
-      if (binding.arg == 'background'){
-        el.style.backgroundColor = binding.value;
-      } else {
-        el.style.color = binding.value;
-      }
-    }, delay);
-
-  }
-});
+Vue.filter('to-lowercase')
 
 new Vue({
   render: h => h(App),
