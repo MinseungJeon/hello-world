7a0a484809365835814e9ccd84eb7a5aaf3e8b79
전민승 <jeon@jeonminseungs-MacBook-Pro.local>
2021-02-16 15:00:30 +0900

    14-12. Using Dynamic Names and Attributes

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,15 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Animation</h1>
         <hr />
+        <select v-model="alertAnimation" class="form-control">
+          <option value="fade">Fade</option>
+          <option value="slide">Slide</option>
+        </select>
         <button class="btn btn-primary" @click="show = !show">
           Show Alert
         </button>
         <br /><br />
-        <transition name="fade">
+        <transition name="alertAnimation">
           <div class="alert alert-info" v-show="show">This is some Info</div>
         </transition>
         <transition name="slide" type="animation" appear>
@@ -30,6 +34,7 @@ export default {
   data() {
     return {
       show: true,
+      alertAnimation: "fade",
     };
   },
 };
