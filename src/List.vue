10a0a1aa126d394afd48873c33ecd8d4aa1cc169
전민승 <jeon@jeonminseungs-MacBook-Pro.local>
2021-02-16 12:05:38 +0900

    13-06. Creating and Using Mixins

--- src/List.vue
+++ src/List.vue
@@ -11,20 +11,9 @@
 </template>
 
 <script>
+import { fruitMixin } from "./fruitMixin";
 export default {
-  data() {
-    return {
-      fruits: ["Apple", "Banana", "Mange", "Melon"],
-      filterText: "",
-    };
-  },
-  computed: {
-    filteredFruits() {
-      return this.fruits.filter((element) => {
-        return element.match(this.filterText);
-      });
-    },
-  },
+  mixins: [fruitMixin],
 };
 </script>
 
