0885dea22afafca869576d56194f4aa00eb8a02a
전민승 <jeon@jeonminseungs-MacBook-Pro.local>
2021-02-16 14:14:59 +0900

    14-03. Preparing Code to use Transitions

--- src/App.vue
+++ src/App.vue
@@ -2,40 +2,26 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Filters & Mixins</h1>
-        <p>{{ text | toUppercase | tolowercase }}</p>
+        <h1>Animation</h1>
         <hr />
-        <button @click="fruits.push('Berries')">Add New Item</button>
-        <input v-model="filterText" />
-        <ul>
-          <li v-for="(fruit, index) in filteredFruits" v-bind:key="index">
-            {{ fruit }}
-          </li>
-        </ul>
-        <app-list></app-list>
+        <button class="btn btn-primary" @click="show = !show">
+          Show Alert
+        </button>
+        <br /><br />
+        <transition>
+          <div class="alert alert-info" v-if="show">This is some Info</div>
+        </transition>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import List from "./List.vue";
-import { fruitMixin } from "./fruitMixin";
-
 export default {
-  mixins: [fruitMixin],
   data() {
     return {
-      text: "Hello there!",
+      show: false,
     };
-  },
-  filters: {
-    toUppercase(value) {
-      return value.toUpperCase();
-    },
-  },
-  components: {
-    appList: List,
   },
 };
 </script>
