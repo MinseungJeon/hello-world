bb6eebfc06b4e3a19944ecf7a1bc579cb8f5326c
전민승 <jeon@jeonminseungs-MacBook-Pro.local>
2021-02-17 11:19:59 +0900

    16-08. Setting Up Route Parameters

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
     <router-link exact to="/" tag="li" active-class="active"
       ><a>Home</a></router-link
     >
-    <router-link to="user" tag="li" active-class="active"
+    <router-link to="user/10" tag="li" active-class="active"
       ><a>User</a></router-link
     >
   </ul>
