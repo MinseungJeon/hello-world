c2496a4dd26ef3fb136147a3bb188e0a743a90e3
전민승 <jeon@jeonminseungs-MacBook-Pro.local>
2021-02-17 11:33:06 +0900

    16-10. Reacting to Changes in Route Parameters

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,8 +3,11 @@
     <router-link exact to="/" tag="li" active-class="active"
       ><a>Home</a></router-link
     >
-    <router-link to="user/10" tag="li" active-class="active"
-      ><a>User</a></router-link
+    <router-link exact to="user/1" tag="li" active-class="active"
+      ><a>User 1</a></router-link
+    >
+    <router-link exact to="user/2" tag="li" active-class="active"
+      ><a>User 2</a></router-link
     >
   </ul>
 </template>
