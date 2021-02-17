8afc336aaa72a212638dcf9400af95d774c808c4
전민승 <jeon@jeonminseungs-MacBook-Pro.local>
2021-02-17 15:06:32 +0900

    17-04. Using the Centrallized State

--- src/main.js
+++ src/main.js
@@ -1,31 +1,10 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router';
-import App from './App.vue'
-import { routes } from './routes';
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.use(VueRouter);
-
-const router = new VueRouter({
-  routes,
-  mode: 'history',
-  scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition;
-    }
-    if (to.hash){
-      return { selector: to.hash }
-    }
-    return {x: 0, y: 0};
-  }
-});
-
-router.beforeEach((to, from, next) => {
-  console.log('global beforeEach');
-  next();
-});
+import { store } from './store/store';
 
 new Vue({
-  router,
-  render: h => h(App),
-}).$mount('#app')
-
+  el: '#app',
+  store,
+  render: h => h(App)
+})
