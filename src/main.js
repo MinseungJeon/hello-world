cb602739b1abea3b6c67e6a7418dc5ea49a3e9cf
전민승 <jeon@jeonminseungs-MacBook-Pro.local>
2021-02-17 10:47:21 +0900

    16-03. Setting Up and Loading Routes

--- src/main.js
+++ src/main.js
@@ -1,31 +1,16 @@
 import Vue from 'vue'
-import VueResource from 'vue-resource'
+import VueRouter from 'vue-router';
 import App from './App.vue'
-// import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-// import 'bootstrap/dist/css/bootstrap.css'
-// import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { routes } from './routes';
 
-// // Make BootstrapVue available throughout your project
-// Vue.use(BootstrapVue)
-// // Optionally install the BootstrapVue icon components plugin
-// Vue.use(IconsPlugin)
+Vue.use(VueRouter);
 
-// Vue.config.productionTip = false
-
-Vue.use(VueResource);
-
-Vue.http.options.root = "https://vuejs-http-9afd6-default-rtdb.firebaseio.com/";
-Vue.http.interceptors.push((request, next) => {
-  console.log(request);
-  if (request.method == 'POST'){
-    request.method = 'PUT'
-  }
-  next(response => {
-    response.json = () => { return {messages: response.body}}
-  });
-})
+const router = new VueRouter({
+  routes
+});
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
 
