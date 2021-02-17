dc8a149868970a0d3075e538d7b55ca32ca83331
전민승 <jeon@jeonminseungs-MacBook-Pro.local>
2021-02-17 15:34:10 +0900

    17-08. Mapping Getters to Properties

--- src/components/AnotherResult.vue
+++ src/components/AnotherResult.vue
@@ -1,15 +1,15 @@
 <template>
-  <p>Counter is: {{ counter }}</p>
+  <div>
+    <p>Counter is: {{ doubleCounter }}</p>
+    <p>Number of Clicks: {{ stringCounter }}</p>
+  </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 export default {
-  computed: {
-    counter() {
-      return this.$store.getters.doubleCounter;
-    },
-  },
+  computed: { ...mapGetters(["doubleCounter", "stringCounter"]) },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style></style>
