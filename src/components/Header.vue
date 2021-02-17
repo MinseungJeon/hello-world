eb444ade183405c305840836119750980ca50475
전민승 <jeon@jeonminseungs-MacBook-Pro.local>
2021-02-17 11:13:55 +0900

    16-06. Where am I - Styling Active Links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,10 @@
 <template>
   <ul class="nav nav-pills">
-    <li role="presentation"><router-link to="/">Home</router-link></li>
-    <li role="presentation"><router-link to="user">User</router-link></li>
+    <router-link exact to="/" tag="li" active-class="active"
+      ><a>Home</a></router-link
+    >
+    <router-link to="user" tag="li" active-class="active"
+      ><a>User</a></router-link
+    >
   </ul>
 </template>
