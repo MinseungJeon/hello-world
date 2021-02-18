fb778d79e00e41dba7e922576886763bc37435eb
전민승 <jeon@jeonminseungs-MacBook-Pro.local>
2021-02-18 17:29:08 +0900

    21-21. Slots and Dynamic Components

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,47 @@
 <template>
     <div class="container">
-        <app-header></app-header>
         <div class="row">
             <div class="col-xs-12">
-                <transition name="slide" mode="out-in">
-                    <router-view></router-view>
-                </transition>
+                <br>
+                <button class="btn btn-primary" @click="selectedComponent = 'appBlue'">Load Blue Template</button>
+                <button class="btn btn-success" @click="selectedComponent = 'appGreen'">Load Green Template</button>
+                <button class="btn btn-danger" @click="selectedComponent = 'appRed'">Load Red Template</button>
+                <hr>
+                <component :is="selectedComponent">
+                    <p>This is the Content</p>
+                </component>
+                <!--<app-blue>-->
+                    <!--<p>This is the Content</p>-->
+                <!--</app-blue>-->
+                <!--<app-green>-->
+                    <!--<p>This is the Content</p>-->
+                <!--</app-green>-->
+                <!--<app-red>-->
+                    <!--<p>This is the Content</p>-->
+                <!--</app-red>-->
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import Header from './components/Header.vue';
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
     export default {
-        components: {
-            appHeader: Header
+        data: function() {
+          return {
+              selectedComponent: 'appBlue'
+          }
         },
-        created() {
-            this.$store.dispatch('initStocks');
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
         }
     }
 </script>
 
 <style>
-    body {
-        padding: 30px;
-    }
-
-    .slide-enter-active {
-        animation: slide-in 200ms ease-out forwards;
-    }
-
-    .slide-leave-active {
-        animation: slide-out 200ms ease-out forwards;
-    }
-
-    @keyframes slide-in {
-        from {
-            transform: translateY(-30px);
-            opacity: 0;
-        }
-        to {
-            transform: translateY(0);
-            opacity: 1;
-        }
-    }
-
-    @keyframes slide-out {
-        from {
-            transform: translateY(0);
-            opacity: 1;
-        }
-        to {
-            transform: translateY(-30px);
-            opacity: 0;
-        }
-    }
 </style>
-
