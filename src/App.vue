755e9a3150eb068c7f87c95d362a50dfb9dcdbe5
전민승 <jeon@jeonminseungs-MacBook-Pro.local>
2021-02-19 10:18:34 +0900

    22-05. Sending a POST Request

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,23 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Directives Exercise</h1>
-        <!-- Exercise -->
-        <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
-        <button v-customOn:click="clicked" class="btn btn-primary">
-          Click Me
-        </button>
-        <hr />
-        <div
-          style="width: 100px; heightL 100px; background-color: lightgreen"
-          v-customOn:
-          mouseenter="mouseEnter"
-          mouseleave="mouseLeave"
-        ></div>
-      </div>
-    </div>
+  <div id="app">
+    <app-header />
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  directive: {
-    customOn: {
-      bind(el, binding) {
-        // el.onClick = function() {
-        //   binding.value();
-        // };
-        const type = binding.arg;
-        const fn = binding.value;
-        el.addEventListener(type, fn);
-      },
-    },
-  },
-  methods: {
-    clicked() {
-      alert("I was clicked");
-    },
-    mouseEnter() {
-      console.log("Mouse entered!");
-    },
-    mouseLeave() {
-      console.log("Mouse leaved!");
-    },
-  },
-};
+  import Header from './components/header/header.vue'
+  export default {
+    name: 'app',
+    components: {
+      'app-header': Header
+    }
+  }
 </script>
 
-<style></style>
+<style>
+  body, html {
+    margin: 0;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  }
+</style>
