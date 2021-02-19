051f34458885228fe343d61445a246558217ec26
전민승 <jeon@jeonminseungs-MacBook-Pro.local>
2021-02-19 10:48:05 +0900

    22-09. Using Interceptors

--- src/main.js
+++ src/main.js
@@ -9,6 +9,18 @@ axios.defaults.baseURL = 'https://vue-update-14f20-default-rtdb.firebaseio.com'
 axios.defaults.headers.common['Authorization'] = 'fastdsa'
 axios.defaults.headers.get['Accepts'] = 'application/json'
 
+const reqInterceptor = axios.interceptors.request.use(config => {
+  console.log('Request Interceptor', config)
+  return config
+})
+const resInterceptor = axios.interceptors.response.use(res => {
+  console.log('Response Interceptor', res)
+  return res
+})
+
+axios.interceptors.request.eject(reqInterceptor)
+axios.interceptors.request.eject(resInterceptor)
+
 new Vue({
   el: '#app',
   router,
